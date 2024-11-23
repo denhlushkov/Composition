478f6831c9fd44b6f17f3cea8181a05f82963b76
ITrue-dev <den.glushkov.kv@gmail.com>
2024-11-23 14:44:17 +0200

    first

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+    for(const key of fns)
+    {
+        if(typeof key !== 'function')
+        {
+            throw new Error('Not all elements are function!');
+        }
+    }
+    return (x) => fns.reduce((acc, g) => g(acc), x)
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,31 @@
 'use strict';
 
-const compose = (...fns) => (x) => null;
+const compose = (...fns) => {
+    const trash = [];
+    const com = (x) => {
+        if(fns.length === 0) return x;        
+        const end = fns.length - 1;
+        let fin = x;
+
+        try
+        {
+            for(let i = end; i >= 0; --i){
+                fin = fns[i](fin);
+            }
+            return fin;
+        }catch(error) 
+        {
+            for(const num of trash)
+            {
+                num(error);
+            }
+            return undefined;
+        }
+    };
+    com.on = (id, num) => {
+        if(id === 'error') trash.push(num);
+    };
+    return com;
+};
 
 module.exports = { compose };
